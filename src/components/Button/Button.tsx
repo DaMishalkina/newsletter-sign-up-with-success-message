924e1fb91e015a74dc5f142095c61589c726951e
Daria Mishalkina <dashaio251@gmail.com>
2023-07-14 20:19:06 +0200

    fixing output for safari

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState, useEffect, useRef} from "react";
+import classNames from "classnames";
 import "./Button.css";
 
 interface Props {
@@ -13,8 +14,35 @@ export const Button = ({
                            onClick,
                            type = "button",
                            disabled = false}: Props) => {
+    //Fixing focus for safari
+    const [isClicked, setIsClicked] = useState(false);
+    const handleClick = () => {
+        setIsClicked(true);
+        typeof onClick !== "undefined" && onClick();
+    }
+    const ref = useRef<any>(null);
+
+    useEffect(() => {
+        function handleClickOutside(event: Event) {
+            if (ref.current && !ref.current?.contains(event?.target)) {
+                setIsClicked(false);
+            }
+        }
+        document.addEventListener("click", handleClickOutside);
+        return () => {
+            document.removeEventListener("click", handleClickOutside);
+        };
+    }, [ref]);
     return (
-        <button className="button" disabled={disabled} type={type} onClick={onClick}>
+        <button
+            ref={ref}
+            className={classNames("button",
+                isClicked ? "button--active" : "button--inactive"
+                )}
+            disabled={disabled}
+            type={type}
+            onClick={handleClick}
+        >
             <span>{children}</span>
         </button>
     )
--- src/components/svg/IconSuccess.tsx
+++ src/components/svg/IconSuccess.tsx
@@ -5,11 +5,13 @@ import {SvgProps} from "./types/type";
 export const IconSuccess = ({width = "64", height = "64", className = ""}: SvgProps) => {
     return (
         <svg
+            role="img"
             className={className}
              xmlns="http://www.w3.org/2000/svg"
              width={width}
              height={height}
              viewBox="0 0 64 64">
+            <title>Success Orange Icon</title>
             <defs>
                 <linearGradient id="a" x1="100%" x2="0%" y1="0%" y2="100%">
                     <stop offset="0%" stopColor="#FF6A3A"/>
